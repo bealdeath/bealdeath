79abaf93ee2500450194843d65ee3ef72dda27a7
bealdeath <bealdeath@yahoo.ca>
2024-07-25 21:14:05 -0400

    Initial commit

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('postgres://postgres:Andy12987%21%40%23@localhost:5432/Magictable', {
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
--- test.js
+++
@@ -1,2 +0,0 @@
-require('dotenv').config();
-console.log('JWT_SECRET:', process.env.JWT_SECRET);
